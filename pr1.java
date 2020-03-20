cb9361eabc0219122696b86ed2e1849dda0061ac
MinJae <guswlfahs123@naver.com>
2020-03-20 12:57:06 +0900

    pr1.java 산술연산자 사용

---
+++ pr1.java
@@ -0,0 +1,16 @@
+package JAVA;
+
+public class Learn {
+	public static void main(String[] args) {
+		int mathScore=100;
+		int engScore=80;
+		
+		int totalScore=mathScore+engScore;
+		System.out.println(totalScore);
+		
+		double avgScore=totalScore/2.0 ; //형변환
+		System.out.println(avgScore); 
+	}
+	
+}
+
