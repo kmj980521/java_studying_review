c897d101bea1b852b3e7286a4434eede92ec491f
MinJae <guswlfahs123@naver.com>
2020-03-21 14:41:48 +0900

    pr2.java 두 개의 클래스 생성, 생성자, 참조연산자.사용

---
+++ pr2.java
@@ -0,0 +1,38 @@
+package JAVA;
+
+public class LearnJava {
+	int studentID;
+	String studentName;
+	int grade;
+	String address;
+	LearnJava()
+	{
+		
+	}
+	LearnJava(int a, String b,int c,String d)
+	{
+		studentID=a;
+		studentName=b;
+		grade=c;
+		address=d;
+	}
+	public void setStudent(int a, String b,int c,String d)
+	{
+		studentID=a;
+		studentName=b;
+		grade=c;
+		address=d;
+	}
+	public String getStudentName() {
+		return studentName;
+	}
+	public static void main(String[] args) {
+		LearnJava first_student = new LearnJava();
+		first_student.setStudent(101,"자바", 100 , "수원");
+		System.out.println(first_student.getStudentName()); //참보연산자 . 사용
+		
+		LearnJava second_student=new LearnJava(102,"C++",80,"서울");
+		System.out.println(second_student.getStudentName());
+	}
+}
+
