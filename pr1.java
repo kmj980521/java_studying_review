320de5feb0575d152000fe15cb22093bffe536bc
MinJae <guswlfahs123@naver.com>
2020-03-22 12:59:50 +0900

    pr1.java 배열 선언하기

---
+++ pr1.java
@@ -0,0 +1,13 @@
+package JAVA;
+
+public class Example {
+
+	public static void main(String[] args) {
+		int []arr1=new int[10]; // int형 배열이고 요소가 10개인 배열
+		int []arr2;
+		arr2=new int[10];//선언하고 초기화하는 경우에는 new int 생략불가능
+		int []arr3= {101,102,103};//선언과 동시에 초기화하는 경우에는 new int 생략가능 
+	}
+
+}
+
