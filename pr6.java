30c4da3e19b508dcb209cb44e9752cc08092d95a
MinJae <guswlfahs123@naver.com>
2020-03-24 22:10:46 +0900

    pr6.java 인터페이스 선언

---
+++ pr6.java
@@ -0,0 +1,12 @@
+package review5;
+
+public interface Calc {
+	double pi=3.14;
+	int error=-999999999;
+	
+	int add(int num1,int num2);
+	int sub(int num1,int num2);
+	int div(int num1,int num2);
+	int mul(int num1,int num2);
+}
+
