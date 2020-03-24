dd06565b3e4b1e4be69e8524f39e7fb3aaddd179
MinJae <guswlfahs123@naver.com>
2020-03-24 21:48:50 +0900

    pr4.java final예약어 사용. static으로 선언했기 때문에 참조가능

---
+++ pr4.java
@@ -0,0 +1,19 @@
+package JAVA;
+import java.util.ArrayList;
+class define{
+	public static final int min=1;
+	public static final int max=1000;
+	public static final String HI="Good Morning";
+	public static final double PI=3.14;
+}
+public class Instancof {
+
+	public static void main(String[] args) { 
+		System.out.println(define.min);
+		System.out.println(define.max);
+		System.out.println(define.HI);
+		System.out.println(define.PI);
+	}
+	
+}
+
